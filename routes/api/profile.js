c1d6cbe427858f4db2fd62d097d1093575230bcc
Shrey Kumar <shreykr2802@gmail.com>
2020-05-24 08:15:48 +0530

    Rest apis done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 // @route   GET     api/profile/me
 // @desc    Get my profile
@@ -193,6 +195,120 @@ router.put('/experience', [auth, [
         console.error(error.message);
         res.status(500).send('Server Error');
     }
-})
+});
+
+// @route   DELETE     api/profile/experience/:exp_id
+// @desc    Delete profile experience
+// @access  Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        //Get Profile
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        //Get Remove Index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex, 1);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   PUT     api/profile/education
+// @desc    add profile education
+// @access  Private
+
+router.put('/education', [auth, [
+    check('school', 'School is required').not().isEmpty(),
+    check('degree', 'Degree is required').not().isEmpty(),
+    check('fieldofstudy', 'Field Of Study is required').not().isEmpty(),
+    check('from', 'From Date is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.education.unshift(newEdu);
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE     api/profile/education/:edu_id
+// @desc    Delete profile education
+// @access  Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        //Get Profile
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        //Get Remove Index
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+        profile.education.splice(removeIndex, 1);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET     api/profile/github/:username
+// @desc    Get user repos from github
+// @access  Public
+
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            url: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubclientid')}&client_secret=${config.get('githubsecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        }
+
+        request(options, (error, response, body) => {
+            if (error) console.log(error);
+            if(response.statusCode !== 200) {
+                return res.status(404).json({msg: 'No Github profile found!'});
+            }
+
+            res.json(JSON.parse(body));
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
